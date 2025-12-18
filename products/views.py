b5a4dec01134d4ed753b785a4fddc24176d59967
Joseph G <josef.emu@gmail.com>
2025-12-18 22:11:32 +0300

    feat: implemented ProductViewSet with owner-based filtering

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    This ViewSet automatically provides 'list', 'create', 'retrieve',
+    'update', and 'destroy' actions for Products.
+    """
+    serializer_class = ProductSerializer
+    
+    permission_classes = [permissions.IsAuthenticated] # ensures that only loggd-in users can access this endpoint
+  
+    def get_queryset(self):
+        return Product.objects.filter(owner=self.request.user) # user will only see the product they are tracking
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user) # created tracking item (new url POST) sets the 'owner' to current logged-in user
