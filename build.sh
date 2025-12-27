c735a555dd192a61dca68cdcffc4c9ac73492637
Joseph G <josef.emu@gmail.com>
2025-12-27 20:01:05 +0300

    workaround: auto-create superuser

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ python manage.py collectstatic --no-input
 
 # Run database migrations
 python manage.py migrate
+
+# This creates the superuser using the Env Vars you just set.
+# The "|| true" ensures that if the user already exists, the build won't crash.
+python manage.py createsuperuser --noinput || true
