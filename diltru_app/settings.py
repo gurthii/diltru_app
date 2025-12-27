7e98089aa2da58fd51d3a511875cdc81c6f59609
Joseph G <josef.emu@gmail.com>
2025-12-27 05:37:22 +0300

    updated settings for going-live!

--- diltru_app/settings.py
+++ diltru_app/settings.py
@@ -17,8 +17,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['appsbyjoe.pythonanywhere.com', 'localhost', '127.0.0.1'] # going-live
 
 # Application definition
 
