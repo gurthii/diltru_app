b152a64322ed0a1f832986d6bd85f08dab26d9b5
Joseph G <josef.emu@gmail.com>
2025-12-27 23:02:24 +0300

    fix: switched email to port 587

--- diltru_app/settings.py
+++ diltru_app/settings.py
@@ -168,9 +168,9 @@ REST_FRAMEWORK = {
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465 
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587      # 465 
+EMAIL_USE_TLS = True  # False
+EMAIL_USE_SSL = False # True
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')      
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 DEFAULT_FROM_EMAIL = f'dilTru Alerts <{os.getenv("EMAIL_USER")}>'
